bcf8ca78c25c3ade33c0c0a616ee53e0f0ae3994
s110610523 <36975351+honamireiso@users.noreply.github.com>
2019-11-15 07:46:50 +0800

    Update app.js

--- exercise/01-hello/app.js
+++ exercise/01-hello/app.js
@@ -1,9 +1,9 @@
 const Koa = require('koa'); // 引用koa套件
-const app = module.exports = new Koa(); // 建立模組為一個新Koa物件
+const app = module.exports = new Koa(); 
 
 app.use(async function(ctx) { // koa.use 是非同步函式
   console.log('url=', ctx.url) // 紀錄連線網址
   ctx.body = 'Hello World'; // 加入網頁內容
 });
 
-if (!module.parent) app.listen(3000); // 當不是被引用，監聽3000埠
+if (!module.parent)//防止重複 app.listen(3000); // 聽3000埠
