8e83116f2ea95f7519bb7843698bed78d391eca0
s110610523 <36975351+honamireiso@users.noreply.github.com>
2019-11-15 07:52:02 +0800

    Update app.js

--- exercise/04-fileServer/app.js
+++ exercise/04-fileServer/app.js
@@ -4,17 +4,17 @@ const app = new Koa()
 const path = require('path')
 
 app.use(async function(ctx) {
-  const fpath = path.join(__dirname, ctx.path)//有看不懂的地方就"印"!
-  console.log('看看__dirname=', __dirname)// dirname就是根目錄
-  console.log('看看fpath=', fpath)// fpath就是file path檔案路徑,檔案路徑是__dirname跟ctx.path連接起來的結果
+  const fpath = path.join(__dirname, ctx.path)
+  console.log('__dirname=', __dirname)// dirname根目錄
+  console.log('fpath=', fpath)//file path檔案路徑
   const fstat = await fs.promises.stat(fpath)
-  console.log('看看fstat=', fstat)//file state檔案狀態,會印出一大堆的檔案資料
+  console.log('fstat=', fstat)//file state檔案狀態
   if (fstat.isFile()) {
     let type = path.extname(fpath)
-    console.log('看看type=', type)//type是檔案類型(副檔名)
+    console.log('type=', type)//type是檔案類型
     ctx.type = type
-    console.log('看看ctx.type=', ctx.type)//ctx.type會把檔案類型告知瀏覽器,否則類型錯誤可能變成亂碼
-    ctx.body = fs.createReadStream(fpath)// 讀完檔案用串流方式傳回,可以節省記憶體
+    console.log('ctx.type=', ctx.type)//把檔案類型告知瀏覽器
+    ctx.body = fs.createReadStream(fpath)// 讀完檔案用串流方式傳回
   }
 })
 
