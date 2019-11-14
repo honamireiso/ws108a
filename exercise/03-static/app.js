83ff4614b655e92db1133940691849709e71fab9
s110610523 <36975351+honamireiso@users.noreply.github.com>
2019-11-15 07:49:59 +0800

    Update app.js

--- exercise/03-static/app.js
+++ exercise/03-static/app.js
@@ -2,7 +2,7 @@ const serve = require('koa-static');
 const Koa = require('koa');
 const app = new Koa();
 
-app.use(serve(__dirname + '/public'));//確認檔案路徑
+app.use(serve(__dirname + '/public'));//檔案路徑
 
 app.listen(3000);
 
