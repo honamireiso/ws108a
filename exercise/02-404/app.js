595757584cd2a5e7fd1b0f1fe45b9adc2fb4f5e2
s110610523 <36975351+honamireiso@users.noreply.github.com>
2019-11-15 07:49:15 +0800

    Update app.js

--- exercise/02-404/app.js
+++ exercise/02-404/app.js
@@ -5,7 +5,7 @@ const app = module.exports = new Koa();
 app.use(async function pageNotFound(ctx) {
   // we need to explicitly set 404 here
   // so that koa doesn't assign 200 on body=
-  ctx.status = 404//status=資料回應,404=找不到該網址
+  ctx.status = 404//404=找不到該網址
 });
 
-if (!module.parent)/*讓別人無法require到3000*/  app.listen(3000);
+if (!module.parent) app.listen(3000);
