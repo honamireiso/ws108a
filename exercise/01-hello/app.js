b9473ea7e598d11a0835bc7024d8aa208b666c6d
s110610523 <36975351+honamireiso@users.noreply.github.com>
2019-11-15 07:48:06 +0800

    Update app.js

--- exercise/01-hello/app.js
+++ exercise/01-hello/app.js
@@ -3,7 +3,7 @@ const app = module.exports = new Koa();
 
 app.use(async function(ctx) { 
   console.log('url=', ctx.url) // 顯示連線網址
-  ctx.body = 'Hello World'; // 加入網頁內容
+  ctx.body = 'Hello World'; // 網頁內容
 });
 
 if (!module.parent)//防止重複 app.listen(3000); // 聽3000埠
