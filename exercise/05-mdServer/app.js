955e3242416f4b90b0ff37c8a71fab03fc8e2c50
s110610523 <36975351+honamireiso@users.noreply.github.com>
2019-11-15 08:06:50 +0800

    Update app.js

--- exercise/05-mdServer/app.js
+++ exercise/05-mdServer/app.js
@@ -8,25 +8,20 @@ const path = require('path')
 
 app.use(async function(ctx) {
   const fpath = path.join(__dirname, ctx.path)
-  const fstat = await fs.promises.stat(fpath)// 讀取檔案狀態,放在fstat
-  console.log('觀察fpath=', fpath)
-  console.log('觀察fstat=', fstat)
+  const fstat = await fs.promises.stat(fpath)// 讀取檔案狀態
   if (fstat.isFile()) {
     let ext = path.extname(fpath)
-    console.log('觀察ext=', ext)
 
     //判斷副檔名是否為md,是的話轉為html,否則以串流方式傳回檔案內容
     if (ext === '.md') {
-      let md = await fs.promises.readFile(fpath, 'utf8')// 讀取檔案並轉為字串(以utf-8碼)放在md
-      let html = mdit.render(md)// midt模組裡面的render函數將括號內的檔案(在此為md)轉檔(在此即轉為html檔)
+      let md = await fs.promises.readFile(fpath, 'utf8')
+      let html = mdit.render(md)// mdit模組裡面的render函數將括號內的檔案轉檔
       ctx.type = '.html'
       ctx.body = html
     } else {
       ctx.type = ext// ctx是物件,type區塊用以描述檔案屬性
-      console.log('觀察ctx.type', ctx.type)
       ctx.body = fs.createReadStream(fpath)// ctx是物件,body區塊用以放置檔案串流
       //此時ctx裡面有type和body區塊
-      console.log('觀察ctx.body', ctx.body)
     }
   }
 })
