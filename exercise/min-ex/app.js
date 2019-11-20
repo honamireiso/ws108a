fa53155a9651937bacb93f4ddfd89bef3208c2dc
s110610523 <36975351+honamireiso@users.noreply.github.com>
2019-11-20 21:38:04 +0800

    Update app.js

--- exercise/min-ex/app.js
+++ exercise/min-ex/app.js
@@ -1,1 +1,9 @@
-app.use(serve(__dirname + '/public'));
+const serve = require('koa-static');
+const Koa = require('koa');
+const app = new Koa();
+
+app.use(serve(__dirname + '/game'));
+
+app.listen(3000);
+
+console.log('listening on port 3000');
